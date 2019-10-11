25b1d169c017849483d14a77d24d7aaf30d34162
impressiveHen <b02505002@gmail.com>
2019-10-11 01:19:02 -0700

    first commit

---
+++ src/components/about.jsx
@@ -0,0 +1,75 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-about" data-section="about">
+        <div className="colorlib-narrow-content">
+            <div className="row">
+            <div className="col-md-12">
+                <div className="row row-bottom-padded-sm animate-box" data-animate-effect="fadeInLeft">
+                <div className="col-md-12">
+                    <div className="about-desc">
+                    <span className="heading-meta">About</span>
+                    <h2 className="colorlib-heading">Who Am I?</h2>
+                    <p>
+                        I studied at UCSD where I major in Electrical and Computer Engineering. My goal is to become a full-stack software engineer in the U.S. Some 
+                        of the hobbies I enjoy are tennis, comedy, travel, and cooking. With great power comes great responsiblity.
+                    </p>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        </section>
+        <section className="colorlib-about">
+        <div className="colorlib-narrow-content">
+            <div className="row">
+            <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                <span className="heading-meta">Expertise</span>
+                <h2 className="colorlib-heading">What I do?</h2>
+            </div>
+            </div>
+            <div className="row row-pt-md">
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-1">
+                <span className="icon">
+                    <i className="icon-bulb" />
+                </span>
+                <div className="desc">
+                    <h3>Machine Learning and Data Science</h3>
+                    <p>Took courses like Computer Vision, NLP, Deep Learning CNN RNN, Machine Learning algorithms</p>
+                </div>
+                </div>
+            </div>
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-3">
+                <span className="icon">
+                    <i className="icon-phone3" />
+                </span>
+                <div className="desc">
+                    <h3>App Development </h3>
+                    <p>Use JavaScript,React, Java, Android, React Native to construct awesome apps</p>
+                </div>
+                </div>
+            </div>
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-5">
+                <span className="icon">
+                    <i className="icon-data" />
+                </span>
+                <div className="desc">
+                    <h3>Mechanical Design</h3>
+                    <p>Sketch, stimulate, design mechanism using AutoCad, Comsol, SolidWorks</p>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        </section>
+      </div>
+    )
+  }
+}
---
+++ src/components/timeline.jsx
@@ -0,0 +1,79 @@
+import React, { Component } from 'react'
+
+export default class Timeline extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-experience" data-section="timeline">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                <span className="heading-meta">History</span>
+                <h2 className="colorlib-heading animate-box">Timeline</h2>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <div className="timeline-centered">
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-4">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>2erguy Internship <span> 2019 Aug. ~ present</span></h2>
+                        <p>
+                        1. React-Native application deployment, including Redux state control, displaying JSX component, <br/>
+                           fetch Firebase, Google Map API for timely location data. <br/> 
+                        2. Work closely with UI/UX team for application design.
+                        </p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-3">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>TomTom Internship <span> 2019 July. ~ Aug.</span></h2>
+                        <p>
+                        1. Android app development to verify voice guidance system in NavApp using Android Studio. <br/>
+                        2. Java testing applications for Ogg audio file verification using Eclipse. <br/>
+                        3. Git version control using Bitbucket. <br/>
+                        4. Daily Scrum meeting
+                        </p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-5">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>University of California San Diego <span> 2018 Sep. ~ present</span></h2>
+                        <p>Electrical and Computer Engineering - Machine Learning and Data Science</p> 
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-6">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>National Taiwan University <span> 2013 Sep. ~ 2017 Jun.</span></h2>
+                        <p>Mechanical Engineering </p> 
+                      </div>
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
