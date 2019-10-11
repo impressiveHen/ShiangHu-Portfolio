bd5ce6033576191adae5fea4d815769f140ab201
impressiveHen <b02505002@gmail.com>
2019-10-11 01:29:39 -0700

    Updates

---
+++ precache-manifest.6df5075506d8fd200c89e245289a53db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5ce8ba2375747d791ee25acb877a51a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9210d87ba06d2abcdfb8",
+    "url": "/static/css/main.3986cfeb.chunk.css"
+  },
+  {
+    "revision": "268dd5d18c184ac2c1d5",
+    "url": "/static/js/2.a1470b46.chunk.js"
+  },
+  {
+    "revision": "9210d87ba06d2abcdfb8",
+    "url": "/static/js/main.17f9d5f8.chunk.js"
+  },
+  {
+    "revision": "779bb465e39aacfd61fe",
+    "url": "/static/js/runtime-main.63337dba.js"
+  }
+]);
